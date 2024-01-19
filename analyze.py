c00b9f8e6918c33dcea0a61d3fdece483131169e
Dibyanshu Patnaik <dibyanshupatnaik@gmail.com>
2024-01-19 09:49:37 -0500

    Initial commit

--- analyze.py
+++ analyze.py
@@ -1,10 +1,17 @@
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from dotenv import load_dotenv
+import os
 import time
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+
+load_dotenv()
+endpoint = os.getenv('ENDPOINT')
+key = os.getenv('KEY')
+
+endpoint = "https://mlip-lab1-dib.cognitiveservices.azure.com/"
+key = "6823a57c8ff143bb92779ad2980290c0"
 
 credentials = CognitiveServicesCredentials(key)
 
@@ -13,12 +20,15 @@ client = ComputerVisionClient(
     credentials=credentials
 )
 
+
 def read_image(uri):
     numberOfCharsInOperationId = 36
     maxRetries = 10
 
+    print("before sdk call")
     # SDK call
     rawHttpResponse = client.read(uri, language="en", raw=True)
+    print(rawHttpResponse.headers)
 
     # Get ID from returned headers
     operationLocation = rawHttpResponse.headers["Operation-Location"]
@@ -27,23 +37,24 @@ def read_image(uri):
 
     # SDK call
     result = client.get_read_result(operationId)
-    
+
     # Try API
     retry = 0
-    
+
     while retry < maxRetries:
-        if result.status.lower () not in ['notstarted', 'running']:
+        if result.status.lower() not in ['notstarted', 'running']:
             break
         time.sleep(1)
         result = client.get_read_result(operationId)
-        
+
         retry += 1
-    
+
     if retry == maxRetries:
         return "max retries reached"
 
     if result.status == OperationStatusCodes.succeeded:
-        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        res_text = " ".join(
+            [line.text for line in result.analyze_result.read_results[0].lines])
         return res_text
     else:
         return "error"
--- app.py
+++ app.py
@@ -3,29 +3,34 @@ from analyze import read_image
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-# API at /api/v1/analysis/ 
+# API at /api/v1/analysis/
 @app.route("/api/v1/analysis/", methods=['GET'])
 def analysis():
     # Try to get the URI from the JSON
     try:
         get_json = request.get_json()
+        # print("Get JSON: ", get_json)
         image_uri = get_json['uri']
+        print("image_uri: ", image_uri)
     except:
         return jsonify({'error': 'Missing URI in JSON'}), 400
-    
+        # return request.get_json()
+        # return ("'error': 'Missing URI in JSON'"), 400
+
     # Try to get the text from the image
     try:
         res = read_image(image_uri)
-        
+        print("Trying")
         response_data = {
             "text": res
         }
-    
+
         return jsonify(response_data), 200
     except:
         return jsonify({'error': 'Error in processing'}), 500
